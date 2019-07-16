71922890dd989132ab1e3f0b0b5c09bd689cc12c
Ivan Almada <soulfistication@gmail.com>
2019-07-15 23:58:34 -0500

    Initial Commit

---
+++ DLReality/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DLReality
+//
+//  Created by Ivan on 7/15/19.
+//  Copyright © 2019 Ivan. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
